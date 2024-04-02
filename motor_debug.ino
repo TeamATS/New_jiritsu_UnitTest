0454379df6dac36e6449a543898eac9e4b4e9160
okmn55 <93635265+okmn55@users.noreply.github.com>
2024-04-02 11:08:04 +0900

    モータドライバ単体テスト用プログラム

---
+++ motor_debug.ino
@@ -0,0 +1,60 @@
+
+void setup() {
+
+  pinMode(PB12, INPUT_PULLUP);
+  pinMode(PB13, INPUT_PULLUP);
+  pinMode(PB14, INPUT_PULLUP);
+  pinMode(PB15, INPUT_PULLUP);
+  pinMode(PA8, INPUT_PULLUP);
+  pinMode(PA9, INPUT_PULLUP);
+  pinMode(PA10, INPUT_PULLUP);
+  pinMode(PA11, INPUT_PULLUP);
+  pinMode(PA12, INPUT_PULLUP);
+  pinMode(PA15, INPUT_PULLUP);
+  pinMode(PB3, INPUT_PULLUP);
+  pinMode(PB4, INPUT_PULLUP);
+  pinMode(PB5, INPUT_PULLUP);
+  pinMode(PB6, INPUT_PULLUP);
+  pinMode(PB7, INPUT_PULLUP);
+  pinMode(PB8, INPUT_PULLUP);
+  pinMode(PB9, INPUT_PULLUP);
+
+  pinMode(PB2, INPUT_PULLUP);
+  pinMode(PB1, INPUT_PULLUP);
+  pinMode(PB0, INPUT_PULLUP);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PC15, OUTPUT);
+  pinMode(PC14, OUTPUT);
+  pinMode(PC13, OUTPUT);
+
+  pinMode(PA4, OUTPUT); //DIR_R
+  pinMode(PA5, OUTPUT); //DIR_L
+  analogWriteFrequency(1000);
+
+}
+
+void loop() {
+
+  //正転
+  digitalWrite(PA4, HIGH);
+  digitalWrite(PA5, HIGH);
+  analogWrite(PA6, 255); //PWM_R
+  analogWrite(PA7, 255); //PWM_L
+
+  delay(1000);
+
+  //停止
+  analogWrite(PA6, 0); //PWM_R
+  analogWrite(PA7, 0); //PWM_L
+
+  delay(500);
+
+  //逆転
+  digitalWrite(PA4, LOW);
+  digitalWrite(PA5, LOW);
+  analogWrite(PA6, 255); //PWM_R
+  analogWrite(PA7, 255); //PWM_L
+
+  delay(1000);
+}
