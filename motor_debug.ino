0e8dfaae261640c11be383a0e8373b15b0c77039
okmn55 <93635265+okmn55@users.noreply.github.com>
2024-04-03 21:45:22 +0900

    new

--- motor_debug.ino
+++ motor_debug.ino
@@ -36,10 +36,55 @@ void setup() {
   digitalWrite(PC14, LOW);   // turn the LED off by making the voltage LOW
   digitalWrite(PC13, LOW);   // turn the LED off by making the voltage LOW
 
+  pinMode(PA6, OUTPUT); //PWM_R
+  pinMode(PA7, OUTPUT); //PWM_L
+
 }
 
 void loop() {
 
+  //左：正　右：正
+  digitalWrite(PA6, HIGH);  //PWM_R
+  digitalWrite(PA4, HIGH);  //DIR_R
+  digitalWrite(PA7, HIGH);  //PWM_L
+  digitalWrite(PA5, HIGH);  //DIR_L
+  digitalWrite(PC13,HIGH);
+  digitalWrite(PC14,LOW);
+  digitalWrite(PC15,LOW);
+  delay(3000);
+
+  //左：逆　右：逆
+  digitalWrite(PA6, HIGH);  //PWM_R
+  digitalWrite(PA4, LOW);  //DIR_R
+  digitalWrite(PA7, HIGH);  //PWM_L
+  digitalWrite(PA5, LOW);  //DIR_L
+  
+  digitalWrite(PC13,LOW);
+  digitalWrite(PC14,HIGH);
+  digitalWrite(PC15,LOW);
+  delay(3000);
+
+  //左：正　右：逆
+  digitalWrite(PA6, HIGH);  //PWM_R
+  digitalWrite(PA4, LOW);  //DIR_R
+  digitalWrite(PA7, HIGH);  //PWM_L
+  digitalWrite(PA5, HIGH);  //DIR_L
+  digitalWrite(PC13,HIGH);
+  digitalWrite(PC14,HIGH);
+  digitalWrite(PC15,LOW);
+  delay(3000);
+
+  //左：逆　右：正
+  digitalWrite(PA6, HIGH);  //PWM_R
+  digitalWrite(PA4, HIGH);  //DIR_R
+  digitalWrite(PA7, HIGH);  //PWM_L
+  digitalWrite(PA5, LOW);  //DIR_L
+  digitalWrite(PC13,LOW);
+  digitalWrite(PC14,LOW);
+  digitalWrite(PC15,HIGH);
+  delay(3000);
+
+/*
   digitalWrite(PA4, HIGH);
   digitalWrite(PA5, HIGH);
   analogWrite(PA6, 126); //PWM_R
@@ -79,6 +124,8 @@ delay(1000);
   digitalWrite(PC15,HIGH);
 
 delay(1000);
+
+*/
 
 /*
   //正転
