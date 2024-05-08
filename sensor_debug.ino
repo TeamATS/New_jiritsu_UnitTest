b03ffd0d3073182cd6fcca5f6420fbe82277c692
okmn55 <93635265+okmn55@users.noreply.github.com>
2024-05-08 22:19:52 +0900

    照度センサ、ブレードSW論理修正(反転していた)

--- sensor_debug.ino
+++ sensor_debug.ino
@@ -1,6 +1,7 @@
 
 void setup() {
 
+  //敵センサ
   pinMode(PB12, INPUT);
   pinMode(PB13, INPUT);
   pinMode(PB14, INPUT);
@@ -9,21 +10,29 @@ void setup() {
   pinMode(PA9, INPUT);
   pinMode(PA10, INPUT);
   pinMode(PA11, INPUT);
+
+  //白線センサ
+  pinMode(PB6, INPUT);
+  pinMode(PB7, INPUT);
+  pinMode(PB1, INPUT);
+  pinMode(PB0, INPUT);
+
+  //照度センサ(ブレ上)
   pinMode(PA12, INPUT);
   pinMode(PA15, INPUT);
   pinMode(PB3, INPUT);
-  pinMode(PB4, INPUT);
-  pinMode(PB5, INPUT);
-  pinMode(PB6, INPUT);
-  pinMode(PB7, INPUT);
   pinMode(PB8, INPUT);
   pinMode(PB9, INPUT);
 
+  //照度センサ(ブレ下)
+  pinMode(PB4, INPUT);
+  pinMode(PB5, INPUT);
+
+  //審判モジュール
   pinMode(PB2, INPUT);
-  pinMode(PB1, INPUT);
-  pinMode(PB0, INPUT);
   
-  pinMode(LED_BUILTIN, OUTPUT);
+  //デバッグLED
+  // pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PC15, OUTPUT);
   pinMode(PC14, OUTPUT);
   pinMode(PC13, OUTPUT);
@@ -51,21 +60,21 @@ void loop() {
   if(digitalRead(PA11) == 0)  digitalWrite(PC13, HIGH); //D5点灯
 
   //白線センサのデバッグ処理
-  if(digitalRead(PB6) == 1)  digitalWrite(PC14, HIGH); //D4点灯
-  if(digitalRead(PB7) == 1)  digitalWrite(PC14, HIGH); //D4点灯
-  if(digitalRead(PB1) == 1)  digitalWrite(PC14, HIGH); //D4点灯
-  if(digitalRead(PB0) == 1)  digitalWrite(PC14, HIGH); //D4点灯
+  if(digitalRead(PB6) == 1)  digitalWrite(PC15, HIGH); //D4点灯
+  if(digitalRead(PB7) == 1)  digitalWrite(PC15, HIGH); //D4点灯
+  if(digitalRead(PB1) == 1)  digitalWrite(PC15, HIGH); //D4点灯
+  if(digitalRead(PB0) == 1)  digitalWrite(PC15, HIGH); //D4点灯
 
   //照度センサ(ブレ上)のデバッグ処理
-  if(digitalRead(PA12) == 0)  digitalWrite(PC15, HIGH); //D5点灯
-  if(digitalRead(PA15) == 0)  digitalWrite(PC15, HIGH); //D5点灯
-  if(digitalRead(PB3) == 0)  digitalWrite(PC15, HIGH); //D5点灯
-  if(digitalRead(PB8) == 0)  digitalWrite(PC15, HIGH); //D5点灯
-  if(digitalRead(PB9) == 0)  digitalWrite(PC15, HIGH); //D5点灯
+  if(digitalRead(PA12) == 1)  digitalWrite(PC14, HIGH); //D5点灯
+  if(digitalRead(PA15) == 1)  digitalWrite(PC14, HIGH); //D5点灯
+  if(digitalRead(PB3) == 1)  digitalWrite(PC14, HIGH); //D5点灯
+  if(digitalRead(PB8) == 1)  digitalWrite(PC14, HIGH); //D5点灯
+  if(digitalRead(PB9) == 1)  digitalWrite(PC14, HIGH); //D5点灯
 
   //照度センサ(ブレ下)のデバッグ処理
-  if(digitalRead(PB4) == 0)  digitalWrite(PC15, HIGH); //D5点灯
-  if(digitalRead(PB5) == 0)  digitalWrite(PC15, HIGH); //D5点灯
+  if(digitalRead(PB4) == 1)  digitalWrite(PC14, HIGH); //D5点灯
+  if(digitalRead(PB5) == 1)  digitalWrite(PC14, HIGH); //D5点灯
  /*
   if(digitalRead(PB2) == 0)  digitalWrite(LED_BUILTIN, HIGH);
 
