69956a0826328a6b04b27dac869bcea52ff77a5b
fukufukufukukawa <48360473+fukufukufukukawa@users.noreply.github.com>
2025-01-09 21:31:58 +0900

    2連結成功

--- para_serial_165IC_test.ino
+++ para_serial_165IC_test.ino
@@ -8,11 +8,13 @@ const int SL=  15;     // レジスタロードピン(74HC165-SL/PL) GP27
 //参考　http://zattouka.net/GarageHouse/micon/circuit/HC165.htm$0
 
 byte ShiftData ;
+byte ShiftData2 ;
 const int pulseWidth = 1;      // pulse width in microseconds
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println("Hello1!");
 
   pinMode(SL,OUTPUT);
   pinMode(QH,INPUT);
@@ -20,22 +22,28 @@ void setup() {
 
   digitalWrite(SL,HIGH) ;
   digitalWrite(SCK,LOW) ;
+      Serial.println("test3") ;
 
   // ３秒後に開始
   delay(3000);
   // 74HC165入力端子のデータを読込み表示を行う
   ShiftData = ShiftIn(MISO,SCK,SL) ;
   Serial.println(ShiftData, BIN);
-      Serial.println("Ready!") ;
+      Serial.println("test4") ;
 
 }
 
 void loop() {
 // put your main code here, to run repeatedly:
   byte dt ;
+  byte dt2;
+     double  t1;
+     double t2;
   int i;
     delay(500); // this speeds up the simulation
+    //Serial.println("test2") ;
 
+if(0){
    // 74HC165入力端子のデータを読込む
     dt = ShiftIn(QH,CK,SL) ;
      if ( dt != ShiftData ) {
@@ -44,6 +52,31 @@ void loop() {
           Serial.println(dt, BIN) ;
      }
 
+     dt2 =ShiftInNext(QH,CK,SL);
+     if ( dt2 != ShiftData2 ) {
+          // データに変化が有れば表示する
+          ShiftData2 = dt2 ;
+          Serial.println(dt2, BIN) ;
+     }
+     //Serial.println(dt, BIN) ;
+}else{
+    t1 =micros();
+    dt = ShiftIn(QH,CK,SL) ;
+    dt2 =ShiftInNext(QH,CK,SL);
+    t2 = micros();
+    
+     if ( dt != ShiftData  || dt2 != ShiftData2) {
+          // データに変化が有れば表示する
+          ShiftData = dt ;
+                  ShiftData2 = dt2 ;
+          Serial.print(dt, BIN) ;
+          Serial.print(" ") ;
+          Serial.println(dt2, BIN) ;
+          Serial.println(t2-t1) ;
+
+     }
+
+}
 
 }
 
@@ -57,6 +90,7 @@ byte ShiftIn(int dataPin,int clockPin,int loadPin)
      unsigned char x ;
      int  i ;
 
+    //Serial.println("test") ;
      x = 0 ;
      digitalWrite(loadPin, LOW);// 入力端子(A-H)の情報をレジスタに読込めと指示する
       delayMicroseconds( pulseWidth);
@@ -64,6 +98,28 @@ byte ShiftIn(int dataPin,int clockPin,int loadPin)
 
      x = x | (digitalRead(dataPin) << 7) ;      // H端子の内容を受信する
      for (i=6 ; i>=0 ; i--) { // G端子～A端子まで繰り返す
+          digitalWrite(clockPin, HIGH);// 1ビットシフト指示を出す
+          delayMicroseconds( pulseWidth);
+          
+          x = x | (digitalRead(dataPin) << i) ;      // シフトされた内容を受信する
+          digitalWrite(clockPin, LOW);
+          delayMicroseconds( pulseWidth);
+     }
+     return x ;
+}
+
+byte ShiftInNext(int dataPin,int clockPin,int loadPin)
+{
+     unsigned char x ;
+     int  i ;
+
+    //Serial.println("test") ;
+     x = 0 ;
+     //digitalWrite(loadPin, LOW);// 入力端子(A-H)の情報をレジスタに読込めと指示する
+     // delayMicroseconds( pulseWidth);
+     //digitalWrite(loadPin, HIGH);
+
+     for (i=7 ; i>=0 ; i--) { // G端子～A端子まで繰り返す
           digitalWrite(clockPin, HIGH);// 1ビットシフト指示を出す
           delayMicroseconds( pulseWidth);
           
