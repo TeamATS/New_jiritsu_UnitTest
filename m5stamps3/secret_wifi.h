3522b7910dc0972e318c53638528243f18d6bf27
fukufukufukukawa <48360473+fukufukufukukawa@users.noreply.github.com>
2025-04-17 00:16:51 +0900

    Create secret_wifi.h

---
+++ m5stamps3/secret_wifi.h
@@ -0,0 +1,90 @@
+const char *machineName = "_debug_mode";
+const char *const machineName_1 = "_debug_mode";
+const char *const machineName_matsu = "TUKON M";
+const char *const machineName_koza = "KAGE R";
+const char *const machineName_kana = "TUKON K";
+const char *const machineName_shimo = "HANAYASHA";
+
+
+
+const char *ssid =      "0831A48C3D76-2G";    // robo beya soft bank
+const char *password =  "4kp5hr0tf6gw14"; // ポインタアドレスもconst
+
+
+const char *const ssid0 =      "0831A48C3D76-2G";    // robo beya soft bank
+const char *const password0 =  "4kp5hr0tf6gw14"; // ポインタアドレスもconst
+
+const char *const ssid1 =      "HR02a-2BB4E6";    // robo beya soft bank
+const char *const password1 =  "cztsma0dMd"; // ポインタアドレスもconst
+
+const char *const ssid2 = "ctc-g-8f2dc8";
+const char *const password2 = "56b4d9a65c555"; // ポインタアドレスもconst
+
+const char *const ssid_matsu = "W01_24DF6AD40C18";// white wifi
+const char *const password_matsu = "q4f5g3m7rfyg1em"; // ポインタアドレスもconst
+
+const char *const ssid_koza = "W04_145F947E1480";    // green wifi
+const char *const password_koza = "g4ia9bj2gd0gj0j"; // green wifi
+
+const char *const ssid_kana = "W04_145F947E1480";    // green wifi
+const char *const password_kana = "g4ia9bj2gd0gj0j"; // green wifi
+const char *const ssid_shimo = "W04_145F947E1480";    // green wifi
+const char *const password_shimo = "g4ia9bj2gd0gj0j"; // green wifi
+
+const char *mqtt_server = "192.168.100.50"; // white_wifi
+const char *const mqtt_server1 = "192.168.0.18";
+const char *const mqtt_server2 = "192.168.0.18";
+const char *const mqtt_server_matsu = "192.168.100.50"; // white wifi
+const char *const mqtt_server_koza = "192.168.100.50";  // green wifi
+const char *const mqtt_server_kana = "192.168.100.50"; // white wifi
+const char *const mqtt_server_shimo = "192.168.100.50";  // green wifi
+
+// const char* mqtt_server2 = "192.168.0.17";
+
+// const char *PubTopic    = "/tmr";            // Topic to publish //ToukonM Remotecontrol
+const char *PubTopic = "/robo_remo";         // Topic to publish //
+const char *PubTopic_soushin = "/robo_remo"; // Topic to publish //
+const char *SubTopic = "/rm";
+
+const char *const PubTopic_1 = "/debug_rma"; // Topic to publish
+const char *const PubTopic_matsu = "/tm_rm"; // Topic to publish
+const char *const PubTopic_koza =  "/kr_rma";  // Topic to publish
+
+const char *const SubTopic_1 = "/debug_rm"; // Topic to subscribe
+const char *const SubTopic_matsu = "/tm_rm"; // Topic to subscribe
+const char *const SubTopic_koza = "/kr_rm";  // Topic to subscribe
+
+const char *MQTT_M = "machine";
+const char *const MQTT_M_1 = "debug_mech";
+const char *const MQTT_M_TM = "toukonM_mech";
+const char *const MQTT_M_KR = "kagemushaR_mech";
+
+const char *MQTT_U = "robo";
+const char *const MQTT_U_TM = "robo";
+const char *const MQTT_U_KR = "robo";
+
+const char *MQTT_P = "hogehoge";
+const char *const MQTT_P_TM = "hogehoge";
+const char *const MQTT_P_KR = "hogehoge";
+
+
+
+const char *SubTopic0    = "/robo_remo";                    // Topic to subscribe
+const char *SubTopic1    = "/tm_rm";                    // Topic to subscribe
+const char *SubTopic2    = "/kr_rm";                    // Topic to subscribe
+const char *SubTopic3    = "/tk_rm";                    // Topic to subscribe
+const char *SubTopic4    = "/hy_rm";                    // Topic to subscribe
+
+const char *PubTopic0    = "/robo_remoa";                    // Topic to publish
+const char *PubTopic1    = "/tm_rma";                    // Topic to publish
+const char *PubTopic2    = "/kr_rma";                    // Topic to publish
+const char *PubTopic3    = "/tk_rma";                    // Topic to publish
+const char *PubTopic4    = "/hy_rma";                    // Topic to publish
+
+const char *PubTopic_s1    = "/s1";
+const char *PubTopic_s2    = "/s2";
+const char *PubTopic_s3    = "/s3";
+
+const char *PubTopic_d1    = "/d1";
+const char *PubTopic_d2    = "/d2";
+const char *PubTopic_d3    = "/d3";
